778be2f18e82dad44686a5c27ca523f425577f29
Mr. kuldeep <kkharoliya20@gmail.com>
2024-07-04 11:22:58 +0530

    ENV UNDERSTANDING

---
+++ config/envconfig.js
@@ -0,0 +1,10 @@
+
+const config = {
+    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDbId : String(import.meta.env.VITE_APPWRITE_DB_ID),
+    appwriteCollection: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucket : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+export default config;

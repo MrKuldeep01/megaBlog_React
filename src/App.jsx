a54b9273e4e995226a12d41012ce3f4d3efded73
Mr. kuldeep <kkharoliya20@gmail.com>
2024-07-04 10:01:56 +0530

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='bg-rose-600 text-white text-center p-4 font-semibold rounded'>hello my dear ...</h1>
+    </>
+  )
+}
+
+export default App

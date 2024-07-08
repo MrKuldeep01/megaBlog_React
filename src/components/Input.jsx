1e3a68f7e6d39141c76debded3ec50d1513f9985
Mr. kuldeep <kkharoliya20@gmail.com>
2024-07-08 23:55:38 +0530

    forward ref hook

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,18 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useId } from "react";
 
-const input = forwardRef(({
-    
-}) => {
-  return (
-    <div>
-      
-    </div>
-  )
-})
+const input = forwardRef(
+  (
+    { label, className = "px-4 py-2 rounded-md", type = "text", ...props },
+    ref
+  ) => {
+    const id = useId();
+    return (
+      <div className="w-full">
+        {label && <label htmlFor="id" className="inline-block mb-1 px-1">{label}</label>}
+        <input id={id} type={type} className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`} {...props} ref={ref} />
+      </div>
+    );
+  }
+);
 
-export default Input
+export default Input;

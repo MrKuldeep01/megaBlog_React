99e2562bb72971900ac3e6cba8cdd28cb3e8f387
Mr. kuldeep <kkharoliya20@gmail.com>
2024-07-16 11:59:51 +0530

    update & create posts���

--- src/components/PostForm/PostForm.jsx
+++ src/components/PostForm/PostForm.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
-import { Input, Button,Logo,  } from '../index'
-const PostForm = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import React from "react";
+import { Input, Button, SelectBtn, RTE } from "../index";
+import appwriteService from "../../appwrite/DB_service";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-export default PostForm
+const PostForm = ({ post }) => {
+  const navigate = useNavigate();
+  const { register, handleSubmit, watch, setValue, control, getValues } =
+    useForm({
+      defaultValues: {
+        title: post?.title || "",
+        slug: post?.slug || "",
+        content: post?.content || "",
+        status: post?.status || "active",
+      },
+    });
+  const userData = useSelector((state) => state.user.userData);
+
+  const submitHandler = async (data) => {
+    if (post) {
+      const file = data.image[0]
+        ? appwriteService.uploadFile(data.image[0])
+        : null;
+      if (file) {
+        appwriteService.deleteFile(post.featuredImage);
+      }
+      const Post = await appwriteService.updatePost(post.$id, {
+        ...data,
+        featuredImage: file ? file.$id : undefined,
+      });
+      if (Post) {
+        navigate(`/post/${Post.$id}`);
+      }
+    } else {
+      const file = data.image[0]
+        ? appwriteService.uploadFile(data.image[0])
+        : null;
+      if (file) {
+        const fileid = file.$id;
+        data.featuredImage = fileid;
+        const dbpost = await appwriteService.createPost({
+          ...data,
+          userId: userData.$id,
+        });
+        if (dbpost) {
+          navigate(`/post/${dbpost.$id}`);
+        }
+      }
+    }
+  };
+
+
+  
+  return <div></div>;
+};
+
+export default PostForm;
--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,5 +1,5 @@
-import { Editor } from "@tinymce/tinymce-react";
 import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
 import { Controller } from "react-hook-form";
 
 const RTE = ({ name, control, label, defaultValue = "" }) => {

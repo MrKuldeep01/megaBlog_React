331debde264414019478873110d4118bdde2f3b0
Mr. kuldeep <kkharoliya20@gmail.com>
2024-07-16 19:20:41 +0530

    mind about to blow

--- src/components/PostForm/PostForm.jsx
+++ src/components/PostForm/PostForm.jsx
@@ -64,16 +64,62 @@ const PostForm = ({ post = null }) => {
 
   useEffect(() => {
     const subscription = watch((value, { name }) => {
-      if (name === "title") {
-        setValue("slug", createSlug(value.title,{shouldValidate:true}));
-      }
+        if (name === "title") {
+            setValue("slug", slugTransform(value.title), { shouldValidate: true });
+        }
     });
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, [watch, createSlug, setValue]);
 
-  return <div></div>;
-};
+    return () => subscription.unsubscribe();
+}, [watch, slugTransform, setValue]);
 
-export default PostForm;
+return (
+    <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+        <div className="w-2/3 px-2">
+            <Input
+                label="Title :"
+                placeholder="Title"
+                className="mb-4"
+                {...register("title", { required: true })}
+            />
+            <Input
+                label="Slug :"
+                placeholder="Slug"
+                className="mb-4"
+                {...register("slug", { required: true })}
+                onInput={(e) => {
+                    setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true });
+                }}
+            />
+            <RTE label="Content :" name="content" control={control} defaultValue={getValues("content")} />
+        </div>
+        <div className="w-1/3 px-2">
+            <Input
+                label="Featured Image :"
+                type="file"
+                className="mb-4"
+                accept="image/png, image/jpg, image/jpeg, image/gif"
+                {...register("image", { required: !post })}
+            />
+            {post && (
+                <div className="w-full mb-4">
+                    <img
+                        src={appwriteService.getFilePreview(post.featuredImage)}
+                        alt={post.title}
+                        className="rounded-lg"
+                    />
+                </div>
+            )}
+            <Select
+                options={["active", "inactive"]}
+                label="Status"
+                className="mb-4"
+                {...register("status", { required: true })}
+            />
+            <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full">
+                {post ? "Update" : "Submit"}
+            </Button>
+        </div>
+    </form>
+);
+}
+export default PostForm
